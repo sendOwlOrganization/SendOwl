75cc644b1022183b78ccf273ca1d331b47537b23
pis03133 <pis03133@naver.com>
2022-03-23 16:12:44 +0900

    feat: construct Cate Entity

---
+++ src/main/java/com/example/sendowl/entity/Cate.java
@@ -0,0 +1,27 @@
+package com.example.sendowl.entity;
+
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@DynamicInsert
+@Table(name="tb_cate")
+public class Cate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String cateName;
+
+    private int orderSeq;
+
+}
