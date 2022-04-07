7ebc88665aea32e6e2ebdf9984c3cb8ead42bd2c
Dev-Guccin <ghks2047@naver.com>
2022-04-07 15:00:02 +0900

    fix: delete @ResponseStatus because using CustomizedResponseEntityExceptionHandler

--- src/main/java/com/example/sendowl/excption/BoardNotFoundException.java
+++ src/main/java/com/example/sendowl/excption/BoardNotFoundException.java
@@ -3,7 +3,6 @@ package com.example.sendowl.excption;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class BoardNotFoundException extends RuntimeException{
     public BoardNotFoundException(String msg) {
         super(msg);
--- src/main/java/com/example/sendowl/excption/MemberNotFoundException.java
+++ src/main/java/com/example/sendowl/excption/MemberNotFoundException.java
@@ -3,7 +3,6 @@ package com.example.sendowl.excption;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class MemberNotFoundException extends RuntimeException{
     public MemberNotFoundException(String msg) {
         super(msg);
