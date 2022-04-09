859b7060fa9908fb2bbaeafd63e0caa7a9f2086a
hjoon <guswns3371@gmail.com>
2022-04-10 02:42:17 +0900

    Fix(swagger) : fix configuration about swagger

--- src/main/java/com/example/sendowl/config/SwaggerConfig.java
+++ src/main/java/com/example/sendowl/config/SwaggerConfig.java
@@ -3,36 +3,57 @@ package com.example.sendowl.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.http.HttpHeaders;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Configuration
-//@EnableSwagger2
-@EnableWebMvc
+@EnableSwagger2
 public class SwaggerConfig {
+
+    private static final String API_NAME = "SendOwl API";
+    private static final String API_VERSION = "0.0.1";
+    private static final String API_DESCRIPTION = "SendOwl API 명세서";
+
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.OAS_30)
-                .useDefaultResponseMessages(false)
+        Parameter parameterBuilder = new ParameterBuilder()
+                .name(HttpHeaders.AUTHORIZATION)
+                .description("Access Token")
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .required(false)
+                .build();
+
+        List<Parameter> globalParamters = new ArrayList<>();
+        globalParamters.add(parameterBuilder);
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .globalOperationParameters(globalParamters)
+                .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.example.sendowl"))
                 .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
+                .build();
     }
 
-    private ApiInfo apiInfo() {
+    public ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Practice Swagger")
-                .description("practice swagger config")
-                .version("1.0")
+                .title(API_NAME)
+                .version(API_VERSION)
+                .description(API_DESCRIPTION)
                 .build();
     }
 }
