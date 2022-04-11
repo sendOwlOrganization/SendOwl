d9a251d2a1af783eebb6ccb88db924682e32b4a8
Dev-Guccin <ghks2047@naver.com>
2022-04-11 15:06:51 +0900

    feat: create shadowkey for expired event #25

--- src/main/java/com/example/sendowl/api/service/BoardService.java
+++ src/main/java/com/example/sendowl/api/service/BoardService.java
@@ -8,9 +8,11 @@ import com.example.sendowl.domain.user.repository.UserRepository;
 import com.example.sendowl.entity.RedisBoard;
 import com.example.sendowl.repository.RedisBoardRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static com.example.sendowl.domain.board.exception.enums.BoardErrorCode.*;
 
@@ -22,6 +24,7 @@ public class BoardService {
     private final BoardRepository boardRepository;
     private final UserRepository userRepository;
     private final RedisBoardRepository redisBoardRepository;
+    private final StringRedisTemplate redisTemplate;
 
     public List<Board> getBoardList() {
        boolean active = true;
@@ -42,10 +45,16 @@ public class BoardService {
 
         Board board = boardRepository.findById(id).orElseThrow(()-> new BoardNotFoundException(NOT_FOUND));
 
+        // Redis update 쿼리
         RedisBoard redisBoard = redisBoardRepository.findById(id).orElse(new RedisBoard(id));
         redisBoard.setCount(redisBoard.getCount() + 1);
         redisBoardRepository.save(redisBoard);
 
+        // Redis shadowKey 존재확인
+        if(redisTemplate.opsForValue().get("shadowkey:board:"+id) == null){
+            redisTemplate.opsForValue().set("shadowkey:board:"+id, "");
+            redisTemplate.opsForValue().getAndExpire("shadowkey:board:"+id, 60, TimeUnit.SECONDS);
+        }
         return board;
     }
 }
--- src/main/java/com/example/sendowl/config/RedisConfigure.java
+++ src/main/java/com/example/sendowl/config/RedisConfigure.java
@@ -7,11 +7,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.PatternTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableRedisRepositories
 public class RedisConfigure {
 
     @Value("${spring.redis.host}")
@@ -44,5 +48,12 @@ public class RedisConfigure {
     @Bean
     public PatternTopic topic() {
         return new PatternTopic(pattern);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
     }
 }
