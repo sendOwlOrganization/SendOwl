2170a447dcf58ee965698056f0e4eb62509ea377
hjoon <guswns3371@gmail.com>
2022-04-12 01:49:02 +0900

    Fix: one to many association (User to Board)

--- src/main/java/com/example/sendowl/entity/Category.java
+++ src/main/java/com/example/sendowl/entity/Category.java
@@ -1,6 +1,5 @@
 package com.example.sendowl.entity;
 
-import com.example.sendowl.entity.User.User;
 import lombok.*;
 
 import javax.persistence.*;
@@ -21,7 +20,7 @@ public class Category {
     private String categoryName;
 
     @OneToMany(mappedBy = "category", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    private List<User> userList = new ArrayList<>();
+    private List<Board> boardList = new ArrayList<>();
 
     @Builder
     public Category(String categoryName) {
