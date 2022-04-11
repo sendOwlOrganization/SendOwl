9bd570153b268852e1a5746027df784fb5462bdb
Dev-Guccin <ghks2047@naver.com>
2022-04-11 18:38:39 +0900

    feat: connect mysqldb when expired event subscribed

--- src/main/java/com/example/sendowl/util/RedisMessageSubscriber.java
+++ src/main/java/com/example/sendowl/util/RedisMessageSubscriber.java
@@ -1,7 +1,11 @@
 package com.example.sendowl.util;
 
 
+import com.example.sendowl.entity.Board;
 import com.example.sendowl.entity.RedisBoard;
+import com.example.sendowl.excption.BoardNotFoundException;
+import com.example.sendowl.excption.RedisBoardNotFoundException;
+import com.example.sendowl.repository.BoardRepository;
 import com.example.sendowl.repository.RedisBoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -20,6 +24,8 @@ public class RedisMessageSubscriber implements MessageListener {
 
     @Autowired
     private RedisBoardRepository redisBoardRepository;
+    @Autowired
+    private BoardRepository boardRepository;
 
     @Override
     public void onMessage(Message message, byte[] bytes) { // Callback for processing received objects through Redis.
@@ -28,22 +34,22 @@ public class RedisMessageSubscriber implements MessageListener {
         String channel = new String(message.getChannel(), StandardCharsets.UTF_8);
 
         if(channel.split(":")[1].matches("expired")){
+            System.out.println("Redis expired event!!");
             String[] bodys = body.split(":");
             String key = bodys[0];
             String tag = bodys[1];
             String id = bodys[2];
 
             // 만료된 경우 DB에 조회수를 증가시키는 코드 추가하기
-            System.out.println("find!!!!");
-            System.out.println("body:"+ body);
-            System.out.println("channel:"+ channel);
-            System.out.println("!!"+key + tag + id);
+            Long idL = Long.parseLong(id);
+            RedisBoard redisboard =  redisBoardRepository.findById((idL)).orElseThrow(()->new RedisBoardNotFoundException("해당 게시글 없음"));
+            Long count = redisboard.getCount();
+            Board board = boardRepository.findById(idL).orElseThrow(()-> new BoardNotFoundException("해당 게시글 없음"));
+            board.setHit(board.getHit() + count);
+            boardRepository.save(board);
 
             // 사용완료한 데이터를 제거한다.
-            Long idL = Long.parseLong(id);
-            System.out.println("get:" + redisBoardRepository.findById(idL).get().toString());
             redisBoardRepository.deleteById(idL);
-
             return;
         }
         else{
