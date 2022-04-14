e015af8a48d5a0ac1c4c89ecd313753565fda4e9
hjoon <guswns3371@gmail.com>
2022-04-15 00:11:07 +0900

    Remove(Board): refactor exception handling on board

--- src/main/java/com/example/sendowl/domain/board/exception/BoardNotFoundException.java
+++ src/main/java/com/example/sendowl/domain/board/exception/BoardNotFoundException.java
@@ -1,10 +1,16 @@
 package com.example.sendowl.domain.board.exception;
 
+import com.example.sendowl.common.exception.BaseException;
+import com.example.sendowl.common.exception.enums.BaseErrorCodeIF;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-public class BoardNotFoundException extends RuntimeException{
-    public BoardNotFoundException(String msg) {
-        super(msg);
+public class BoardNotFoundException extends BaseException {
+    public BoardNotFoundException(BaseErrorCodeIF errorCode) {
+        super(errorCode);
+    }
+
+    public BoardNotFoundException(BaseErrorCodeIF errorCode, Throwable throwable) {
+        super(errorCode, throwable);
     }
 }
