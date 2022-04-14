ff21e4b066af186769214c362ab4f23ca79ad060
hjoon <guswns3371@gmail.com>
2022-04-15 01:32:45 +0900

    Refactor: remove RestController annotation on ExceptionHandlers

--- src/main/java/com/example/sendowl/domain/comment/exception/handler/CommentExceptionHandler.java
+++ src/main/java/com/example/sendowl/domain/comment/exception/handler/CommentExceptionHandler.java
@@ -7,11 +7,9 @@ import com.example.sendowl.domain.user.exception.enums.UserErrorCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 
 
-@RestController
 @ControllerAdvice
 public class CommentExceptionHandler {
 
