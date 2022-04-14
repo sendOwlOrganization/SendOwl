df0f8600ee2398dfc27bfc9814c713180859a584
hjoon <guswns3371@gmail.com>
2022-04-15 00:10:57 +0900

    Remove(Comment): refactor exception handling on comment

---
+++ src/main/java/com/example/sendowl/domain/comment/exception/handler/CommentExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.example.sendowl.domain.comment.exception.handler;
+
+import com.example.sendowl.common.dto.BaseErrorResponse;
+import com.example.sendowl.domain.user.exception.UserNotFoundException;
+import com.example.sendowl.domain.user.exception.UserNotValidException;
+import com.example.sendowl.domain.user.exception.enums.UserErrorCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+
+
+@RestController
+@ControllerAdvice
+public class CommentExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class) // 어떤 에러일때 핸들링 할것인지 명시한다.
+    public final ResponseEntity<BaseErrorResponse> handleUserNotFoundExceptions(Exception ex, WebRequest request) {
+        UserNotFoundException boxedException = new UserNotFoundException(
+                UserErrorCode.NOT_FOUND, ex);
+        return new ResponseEntity<>(BaseErrorResponse.createErrorResponse(boxedException),
+                boxedException.getErrorStatus());
+    }
+
+    @ExceptionHandler(UserNotValidException.class)
+    public final ResponseEntity<BaseErrorResponse> handleUserNotValidExceptions(Exception ex, WebRequest request) {
+        UserNotValidException boxedException = new UserNotValidException(
+                UserErrorCode.NOT_FOUND, ex);
+        return new ResponseEntity<>(BaseErrorResponse.createErrorResponse(boxedException),
+                boxedException.getErrorStatus());
+    }
+}
