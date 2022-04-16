667da71091f1817609f3fc4f6c1a9dc50878ce79
hjoon <guswns3371@gmail.com>
2022-04-16 19:03:27 +0900

    Update: Exception Handling Mechanism

--- src/main/java/com/example/sendowl/common/dto/BaseResponseDto.java
+++ src/main/java/com/example/sendowl/common/dto/BaseResponseDto.java
@@ -3,16 +3,23 @@ package com.example.sendowl.common.dto;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BaseResponseDto<T> {
 
-    boolean success;
+    boolean success = true;
+    int statusCode;
     T response;
 
     public BaseResponseDto(T response) {
-        this.success = true;
+        this.statusCode = HttpStatus.OK.value();
+        this.response = response;
+    }
+
+    public BaseResponseDto(T response, HttpStatus statusCode) {
+        this.statusCode = statusCode.value();
         this.response = response;
     }
 
