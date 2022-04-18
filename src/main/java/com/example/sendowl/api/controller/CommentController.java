840db2aa9a76d71b4556f2cd43d19b0ec71cf8ed
Dev-Guccin <ghks2047@naver.com>
2022-04-18 11:19:40 +0900

    fix: for normal operation

--- src/main/java/com/example/sendowl/api/controller/CommentController.java
+++ src/main/java/com/example/sendowl/api/controller/CommentController.java
@@ -4,8 +4,6 @@ package com.example.sendowl.api.controller;
 import com.example.sendowl.domain.comment.dto.CommentRequest;
 import com.example.sendowl.domain.comment.entity.Comment;
 import com.example.sendowl.api.service.CommentService;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -23,13 +21,6 @@ public class CommentController {
 
     // 댓글 등록
     @Operation(summary = "comment insert api", description = "board insert api")
-    @ApiResponses({
-            @ApiResponse(code = 200, message = "OK !!"),
-            @ApiResponse(code = 201, message = "CREATED !!"),
-            @ApiResponse(code = 400, message = "BAD REQUEST !!"),
-            @ApiResponse(code = 404, message = "NOT FOUND !!"),
-            @ApiResponse(code = 500, message = "INTERNAL SERVER ERROR !!")
-    })
     @PostMapping(path = "/comment")
     public ResponseEntity<Comment> insertComment(@RequestBody CommentRequest vo){
         commentService.insertComment(vo);
@@ -38,13 +29,6 @@ public class CommentController {
 
     // 대댓글 등록
     @Operation(summary = "nested comment insert api", description = "board insert api")
-    @ApiResponses({
-            @ApiResponse(code = 200, message = "OK !!"),
-            @ApiResponse(code = 201, message = "CREATED !!"),
-            @ApiResponse(code = 400, message = "BAD REQUEST !!"),
-            @ApiResponse(code = 404, message = "NOT FOUND !!"),
-            @ApiResponse(code = 500, message = "INTERNAL SERVER ERROR !!")
-    })
     @PostMapping(path = "/comment/nest")
     public ResponseEntity<Comment> insertNestedComment(@RequestBody CommentRequest vo){
         commentService.insertNestedComment(vo);
@@ -52,13 +36,6 @@ public class CommentController {
     }
     // 댓글 목록
     @Operation(summary = "comment list api", description = "list api")
-    @ApiResponses({
-            @ApiResponse(code = 200, message = "OK !!"),
-            @ApiResponse(code = 201, message = "CREATED !!"),
-            @ApiResponse(code = 400, message = "BAD REQUEST !!"),
-            @ApiResponse(code = 404, message = "NOT FOUND !!"),
-            @ApiResponse(code = 500, message = "INTERNAL SERVER ERROR !!")
-    })
     @GetMapping(path = "/comment/list")
     public ResponseEntity<List<Comment>> getCommentList(@RequestParam("board-id") Long boardId){
         List<Comment> comments = commentService.selectCommentList(boardId);
