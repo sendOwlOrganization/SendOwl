359bb0ed8bcf395a4ae8d914dddfcaee6795fe17
Dev-Guccin <ghks2047@naver.com>
2022-04-24 15:34:25 +0900

    del: jpql for searching CategoryName Entity

--- src/main/java/com/example/sendowl/domain/category/repository/CategoryRepository.java
+++ src/main/java/com/example/sendowl/domain/category/repository/CategoryRepository.java
@@ -1,17 +1,12 @@
 package com.example.sendowl.domain.category.repository;
 
-
-import com.example.sendowl.domain.board.entity.Board;
 import com.example.sendowl.domain.category.entity.Category;
-import com.example.sendowl.domain.comment.entity.Comment;
+import com.example.sendowl.domain.category.entity.CategoryName;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    @Query(value = "SELECT c FROM Category c WHERE c.categoryName=:categoryName")
-    Optional<Category> findByCategoryName(String categoryName);
+    Optional<Category> findByCategoryName(CategoryName categoryName);
 }
 
