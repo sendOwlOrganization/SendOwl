ade82d23a01adcbc2793001c77f6ec0799091840
Dev-Guccin <ghks2047@naver.com>
2022-04-24 22:53:45 +0900

    feat: categoryDto

--- src/main/java/com/example/sendowl/domain/category/dto/CategoryDto.java
+++ src/main/java/com/example/sendowl/domain/category/dto/CategoryDto.java
@@ -1,2 +1,19 @@
-package com.example.sendowl.domain.category.dto;public class CategoryDto {
+package com.example.sendowl.domain.category.dto;
+
+import com.example.sendowl.domain.category.entity.Category;
+import com.example.sendowl.domain.category.entity.CategoryName;
+import lombok.Data;
+
+public class CategoryDto {
+
+    @Data
+    public static class CategoriesRes{
+        private Long id;
+        private CategoryName categoryName;
+
+        public CategoriesRes(Category entity) {
+            this.id = entity.getId();
+            this.categoryName = entity.getCategoryName();
+        }
+    }
 }
