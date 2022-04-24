425b4adef1958a6bb3228d5620d55287bfffbbb5
Dev-Guccin <ghks2047@naver.com>
2022-04-24 22:52:13 +0900

    fix: AUTH_WHITELIST /api/categories

---
+++ src/main/java/com/example/sendowl/api/controller/CategoryController.java
@@ -0,0 +1,43 @@
+package com.example.sendowl.api.controller;
+
+
+import com.example.sendowl.common.dto.BaseResponseDto;
+import com.example.sendowl.api.service.BoardService;
+import com.example.sendowl.domain.board.entity.Board;
+import com.example.sendowl.domain.user.dto.UserDto;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+import static com.example.sendowl.domain.board.dto.BoardDto.*;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/api/boards")
+public class BoardController {
+
+    private final BoardService boardService;
+
+    @Operation(summary = "list api", description = "list api")
+    @GetMapping(path = "") // 게시글 목록
+    public BaseResponseDto<List<BoardsRes>> boards(){
+        return new BaseResponseDto<List<BoardsRes>>(boardService.getBoardList());
+    }
+
+    @Operation(summary = "board insert api", description = "board insert api")
+    @PostMapping(path = "/board") // 게시글 등록
+    public BaseResponseDto<BoardsRes> board(final @Valid @RequestBody BoardReq rq){
+
+        return new BaseResponseDto<BoardsRes>(boardService.insertBoard(rq));
+    }
+
+    @Operation(summary = "board api", description = "board api")
+    @GetMapping(path = "/{id}") // 게시글 상세
+    public BaseResponseDto<DetailRes> boardDetail(@PathVariable Long id ){
+        return new BaseResponseDto<DetailRes>(boardService.boardDetail(id));
+    }
+
+}
---
+++ src/main/java/com/example/sendowl/domain/category/dto/CategoryDto.java
@@ -0,0 +1,2 @@
+package com.example.sendowl.domain.category.dto;public class CategoryDto {
+}
