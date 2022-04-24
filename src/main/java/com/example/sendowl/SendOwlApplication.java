40c2a1d3c7d12a19f7a6a1ed142de7fc021e7801
Dev-Guccin <ghks2047@naver.com>
2022-04-24 15:38:44 +0900

    feat: insert CategoryName Enum to mysql database

--- src/main/java/com/example/sendowl/SendOwlApplication.java
+++ src/main/java/com/example/sendowl/SendOwlApplication.java
@@ -1,13 +1,38 @@
 package com.example.sendowl;
 
+import com.example.sendowl.domain.category.entity.Category;
+import com.example.sendowl.domain.category.entity.CategoryName;
+import com.example.sendowl.domain.category.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Optional;
+
 @SpringBootApplication
-public class SendOwlApplication {
+@RequiredArgsConstructor
+public class SendOwlApplication implements CommandLineRunner {
+
+    private final CategoryRepository categoryRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SendOwlApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        // 카테고리 인서트하는 로직 적용 => 나중에 삭제
+        for(CategoryName categoryName : CategoryName.values()){
+            Optional<Category> optCategory = categoryRepository.findByCategoryName(categoryName);
+            if(optCategory.isEmpty()){
+                categoryRepository.save(
+                        new Category().builder()
+                                .categoryName(categoryName)
+                                .build()
+                );
+            }
+        }
+    }
 }
+
