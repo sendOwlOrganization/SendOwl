3837e6ae834e9b800321703df2a217a433f251d4
Dev-Guccin <ghks2047@naver.com>
2022-04-24 15:40:32 +0900

    fix: Exception handler when using Enum.valueOf Method & adjust proper ErrorCode

--- src/test/java/com/example/sendowl/repository/CategoryRepositoryTest.java
+++ src/test/java/com/example/sendowl/repository/CategoryRepositoryTest.java
@@ -1,2 +1,22 @@
-package com.example.sendowl.repository;public class CategoryRepositoryTest {
+package com.example.sendowl.repository;
+
+import com.example.sendowl.domain.category.entity.CategoryName;
+import com.example.sendowl.domain.category.repository.CategoryRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class CategoryRepositoryTest {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    public void 카테고리조회가문자열인경우(){
+        categoryRepository.findByCategoryName(CategoryName.FREE);
+    }
+
 }
