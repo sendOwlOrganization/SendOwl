fda97e9c15edf5c0a9f30583352280a1cd68cd16
Dev-Guccin <ghks2047@naver.com>
2022-04-29 11:15:12 +0900

    fix: junit test

--- src/test/java/com/example/sendowl/redis/RedisRepositoryTest.java
+++ src/test/java/com/example/sendowl/redis/RedisRepositoryTest.java
@@ -1,8 +1,6 @@
 package com.example.sendowl.redis;
 
-import com.example.sendowl.redis.entity.RedisBoard;
 import com.example.sendowl.redis.exception.RedisBoardNotFoundException;
-import com.example.sendowl.redis.repository.RedisBoardRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,29 +15,16 @@ import static com.example.sendowl.domain.board.exception.enums.BoardErrorCode.*;
 public class RedisRepositoryTest {
 
     @Autowired
-    private RedisBoardRepository redisBoardRepository;
-    @Autowired
     private StringRedisTemplate redisTemplate;
 
     @Test
     void repoTest() {
-        RedisBoard redisBoard = new RedisBoard(2L);
-
-        redisBoardRepository.save(redisBoard);
-
-        redisBoardRepository.count();
-
         //redisBoardRepository.delete(redisBoard);
 
     }
 
     @Test
     void repoShadowKeyTest() {
-    }
-
-    @Test
-    void DelShadowKeyTest() {
-        redisBoardRepository.deleteById(1L);
     }
 
     @Test
@@ -51,10 +36,5 @@ public class RedisRepositoryTest {
 
     @Test
     void AddBoardCount() {
-
-        RedisBoard redisBoard = redisBoardRepository.findById(1L).orElseThrow(() -> new RedisBoardNotFoundException(NOT_FOUND));
-        redisBoard.setCount(redisBoard.getCount() + 1);
-        redisBoardRepository.save(redisBoard);
-        System.out.println("redisBaordCount : " + redisBoard.getCount());
     }
 }
--- src/test/java/com/example/sendowl/repository/BoardRepositoryTest.java
+++ src/test/java/com/example/sendowl/repository/BoardRepositoryTest.java
@@ -1,22 +1,30 @@
 package com.example.sendowl.repository;
 
-import com.example.sendowl.domain.category.entity.CategoryName;
-import com.example.sendowl.domain.category.repository.CategoryRepository;
+import com.example.sendowl.domain.board.entity.Board;
+import com.example.sendowl.domain.board.repository.BoardRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
 
 @AutoConfigureMockMvc
 @SpringBootTest
-public class CategoryRepositoryTest {
+public class BoardRepositoryTest {
 
     @Autowired
-    private CategoryRepository categoryRepository;
+    private BoardRepository boardRepository;
 
     @Test
-    public void 카테고리조회가문자열인경우(){
-        categoryRepository.findByCategoryName(CategoryName.FREE);
+    public void 컬럼을대입하여쿼리조회(){
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<Board> pages = boardRepository.findByContentContaining(pageable, "s");
+        System.out.println(pages.getTotalPages());
+        for(Board board : pages.getContent()){
+        }
     }
 
 }
