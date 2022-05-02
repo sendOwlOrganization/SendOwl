1b7deb5b5f0644efbbcc46acf169e7d1fe5c0a02
Dev-Guccin <ghks2047@naver.com>
2022-05-02 11:44:35 +0900

    feat: BoardSpecification #54

--- src/main/java/com/example/sendowl/domain/board/specification/BoardSpecification.java
+++ src/main/java/com/example/sendowl/domain/board/specification/BoardSpecification.java
@@ -1,2 +1,48 @@
-package com.example.sendowl.domain.board.specification;public class BoardSpecification {
+package com.example.sendowl.domain.board.specification;
+
+import com.example.sendowl.domain.board.entity.Board;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+public class BoardSpecification {
+    public static Specification<Board> likeTitle(String title){
+        return new Specification<Board>() {
+            @Override
+            public Predicate toPredicate(Root<Board> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+                // 1) like
+                return criteriaBuilder.like(root.get("title"), "%"+title+"%");
+            }
+        };
+    }
+    public static Specification<Board> likeContent(String content){
+        return new Specification<Board>() {
+            @Override //
+            public Predicate toPredicate(Root<Board> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+                // 1) like
+                return criteriaBuilder.like(root.get("content"), "%"+content+"%");
+            }
+        };
+    }
+    public static Specification<Board> likeUserNickName(String nickName){
+        return new Specification<Board>() {
+            @Override //
+            public Predicate toPredicate(Root<Board> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+                // 1) like
+                return criteriaBuilder.like(root.get("user").get("nickName"), "%"+nickName+"%");
+            }
+        };
+    }
+    public static Specification<Board> equalActive(Boolean active){
+        return new Specification<Board>() {
+            @Override
+            public Predicate toPredicate(Root<Board> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+                // 1) like
+                return criteriaBuilder.equal(root.get("active"), active);
+            }
+        };
+    }
 }
