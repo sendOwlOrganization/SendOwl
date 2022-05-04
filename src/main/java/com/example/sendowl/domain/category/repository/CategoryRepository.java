365317cf12baede434bdcca1272672900e61967d
Dev-Guccin <ghks2047@naver.com>
2022-05-05 00:37:27 +0900

    fix: CategoryRepository

--- src/main/java/com/example/sendowl/domain/category/repository/CategoryRepository.java
+++ src/main/java/com/example/sendowl/domain/category/repository/CategoryRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    Optional<Category> findByCategoryName(CategoryName categoryName);
+    Optional<Category> findByCategoryName(String categoryName);
+
+    Optional<Category> existsByCategoryName(String categoryName);
 }
 
