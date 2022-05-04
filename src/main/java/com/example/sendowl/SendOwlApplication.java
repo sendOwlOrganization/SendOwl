1dbf7ba695f7900159089c4f91c4d161e5b4cd92
Dev-Guccin <ghks2047@naver.com>
2022-05-05 00:40:30 +0900

    del: unneccessary Annotation

--- src/main/java/com/example/sendowl/SendOwlApplication.java
+++ src/main/java/com/example/sendowl/SendOwlApplication.java
@@ -12,32 +12,12 @@ import java.util.*;
 
 @SpringBootApplication
 @RequiredArgsConstructor
-public class SendOwlApplication implements CommandLineRunner {
+public class SendOwlApplication {
 
     private final CategoryRepository categoryRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SendOwlApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-//        // 카테고리 인서트하는 로직 적용 => 나중에 삭제
-//        List<Category> listCategory = categoryRepository.findAll();
-//        Map<String,Boolean> tbCategoryName = new HashMap<String,Boolean>();
-//
-//        listCategory.stream().forEach(category -> tbCategoryName.put(category.getCategoryName(), true));
-//
-//        List<Category> emptyCategory = new ArrayList<>();
-//        for(CategoryName categoryName : CategoryName.values()){
-//            if(!tbCategoryName.containsKey(categoryName.getKey())){
-//                emptyCategory.add(new Category().builder().categoryName(categoryName).build());
-//            }
-//        }
-//
-//        if(emptyCategory.size() != 0){
-//            categoryRepository.saveAll(emptyCategory);
-//        }
     }
 }
 
