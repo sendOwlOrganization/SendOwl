b27258673391f594844c63b72f5a3bf9506fb595
Dev-Guccin <ghks2047@naver.com>
2022-05-04 12:05:37 +0900

    fix: delete enum update when application start

--- src/main/java/com/example/sendowl/SendOwlApplication.java
+++ src/main/java/com/example/sendowl/SendOwlApplication.java
@@ -22,22 +22,22 @@ public class SendOwlApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        // 카테고리 인서트하는 로직 적용 => 나중에 삭제
-        List<Category> listCategory = categoryRepository.findAll();
-        Map<String,Boolean> tbCategoryName = new HashMap<String,Boolean>();
-
-        listCategory.stream().forEach(category -> tbCategoryName.put(category.getCategoryName().getKey(), true));
-
-        List<Category> emptyCategory = new ArrayList<>();
-        for(CategoryName categoryName : CategoryName.values()){
-            if(!tbCategoryName.containsKey(categoryName.getKey())){
-                emptyCategory.add(new Category().builder().categoryName(categoryName).build());
-            }
-        }
-
-        if(emptyCategory.size() != 0){
-            categoryRepository.saveAll(emptyCategory);
-        }
+//        // 카테고리 인서트하는 로직 적용 => 나중에 삭제
+//        List<Category> listCategory = categoryRepository.findAll();
+//        Map<String,Boolean> tbCategoryName = new HashMap<String,Boolean>();
+//
+//        listCategory.stream().forEach(category -> tbCategoryName.put(category.getCategoryName(), true));
+//
+//        List<Category> emptyCategory = new ArrayList<>();
+//        for(CategoryName categoryName : CategoryName.values()){
+//            if(!tbCategoryName.containsKey(categoryName.getKey())){
+//                emptyCategory.add(new Category().builder().categoryName(categoryName).build());
+//            }
+//        }
+//
+//        if(emptyCategory.size() != 0){
+//            categoryRepository.saveAll(emptyCategory);
+//        }
     }
 }
 
