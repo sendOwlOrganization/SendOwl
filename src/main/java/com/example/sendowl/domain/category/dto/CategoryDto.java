e657e9b2cf9cbb750c346cd3c11bdc90e72e13fd
Dev-Guccin <ghks2047@naver.com>
2022-05-04 12:07:43 +0900

    feat: Category Dto insert, update, delete

--- src/main/java/com/example/sendowl/domain/category/dto/CategoryDto.java
+++ src/main/java/com/example/sendowl/domain/category/dto/CategoryDto.java
@@ -1,19 +1,53 @@
 package com.example.sendowl.domain.category.dto;
 
 import com.example.sendowl.domain.category.entity.Category;
-import com.example.sendowl.domain.category.entity.CategoryName;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 public class CategoryDto {
 
-    @Data
+    @Getter
+    @NoArgsConstructor
     public static class CategoriesRes{
         private Long id;
-        private CategoryName categoryName;
+        private String categoryName;
 
         public CategoriesRes(Category entity) {
             this.id = entity.getId();
             this.categoryName = entity.getCategoryName();
         }
+    }
+
+    @Getter
+    @NoArgsConstructor
+    public static class CategoryInsertReq {
+        @NotBlank
+        private String categoryName;
+
+        public Category toEntity() {
+            return new Category(categoryName);
+        }
+    }
+
+    @Getter
+    @NoArgsConstructor
+    public static class CategoryUpdateReq {
+        @NotNull
+        private Long id;
+        @NotBlank
+        private String categoryName;
+
+        public Category toEntity() {
+            return new Category(categoryName);
+        }
+    }
+    @Getter
+    @NoArgsConstructor
+    public static class CategoryDeleteReq {
+        @NotNull
+        private Long id;
     }
 }
