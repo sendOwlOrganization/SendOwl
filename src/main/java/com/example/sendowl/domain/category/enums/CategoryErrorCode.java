ec5f0194e520b88335d78d8c61940de75761a506
Dev-Guccin <ghks2047@naver.com>
2022-05-05 00:36:56 +0900

    feat: CategoryErrorCode

--- src/main/java/com/example/sendowl/domain/category/enums/CategoryErrorCode.java
+++ src/main/java/com/example/sendowl/domain/category/enums/CategoryErrorCode.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 public enum CategoryErrorCode implements BaseErrorCodeIF {
 
     NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 카테고리입니다."),
+    ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "이미 존재하는 카테고리입니다."),
     ;
     private final HttpStatus errorStatus;
     private final String errorMessage;
