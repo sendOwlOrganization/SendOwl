43aa9432cdc3729a7d03ebf7d9b795e82249746b
pis03133 <pis03133@naver.com>
2022-05-12 09:40:25 +0900

    fix: fix return value

--- src/main/java/com/example/sendowl/api/service/CategoryService.java
+++ src/main/java/com/example/sendowl/api/service/CategoryService.java
@@ -1,10 +1,6 @@
 package com.example.sendowl.api.service;
 
-import com.example.sendowl.domain.board.dto.BoardDto;
-import com.example.sendowl.domain.board.entity.Board;
-import com.example.sendowl.domain.category.dto.CategoryDto;
 import com.example.sendowl.domain.category.entity.Category;
-import com.example.sendowl.domain.category.enums.CategoryErrorCode;
 import com.example.sendowl.domain.category.exception.CategoryNameAlreadyExistsException;
 import com.example.sendowl.domain.category.exception.CategoryNotFoundException;
 import com.example.sendowl.domain.category.repository.CategoryRepository;
@@ -12,12 +8,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static com.example.sendowl.domain.category.dto.CategoryDto.*;
-import static com.example.sendowl.domain.category.enums.CategoryErrorCode.*;
+import static com.example.sendowl.domain.category.enums.CategoryErrorCode.ALREADY_EXISTS;
+import static com.example.sendowl.domain.category.enums.CategoryErrorCode.NOT_FOUND;
 
 @Service
 @Transactional(readOnly = true)
