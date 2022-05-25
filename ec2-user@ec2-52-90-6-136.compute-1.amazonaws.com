4004c1d63821116a8d8d2ab378fb895165faabc7
Dev-Guccin <ghks2047@naver.com>
2022-05-25 15:38:50 +0900

    fix: Category Constuctor

---
+++ ec2-user@ec2-52-90-6-136.compute-1.amazonaws.com
@@ -0,0 +1,3 @@
+spring:
+  jwt:
+    secret: {AccessToken}
--- src/main/java/com/example/sendowl/domain/category/entity/Category.java
+++ src/main/java/com/example/sendowl/domain/category/entity/Category.java
@@ -30,11 +30,4 @@ public class Category {
     public void setName(String name){
         this.name = name;
     }
-    @Override
-    public String toString() {
-        return "Category{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
 }
