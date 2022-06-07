8320347da89c9b4946f89dccf68de83678abf9ad
pis03133 <pis03133@naver.com>
2022-06-07 10:10:58 +0900

    fix: MarkdownToText

--- src/main/java/com/example/sendowl/common/converter/MarkdownToText.java
+++ src/main/java/com/example/sendowl/common/converter/MarkdownToText.java
@@ -11,15 +11,19 @@ public class MarkdownToText {
     private String refinedText;
 
     public MarkdownToText(String markText){
-        String range = "[^\uAC00-\uD7A30-9a-zA-Z]";
-        String refinedText = markText.replaceAll(range, " ");
+        String specialCharacters = "[^\uAC00-\uD7A30-9a-zA-Z]";
+        // String htmlSyntax = "<[^>]*>";
+
+        String refinedText = markText.replaceAll(specialCharacters, " ");
 
         this.refinedText = refinedText;
     }
 
     public String markDownToText (String markText) {
-        String range = "[^\uAC00-\uD7A30-9a-zA-Z]";
-        String refinedText = markText.replaceAll(range, " ");
+        String specialCharacters = "[^\uAC00-\uD7A30-9a-zA-Z]";
+        // String htmlSyntax = "<[^>]*>";
+
+        String refinedText = markText.replaceAll(specialCharacters, " ");
 
         return refinedText;
     }
