e20c293fa3d1a3d728ee7c845b794228c542d5ce
Dev-Guccin <ghks2047@naver.com>
2022-06-25 17:33:23 +0900

    feat: Oauth2Req

--- src/main/java/com/example/sendowl/domain/user/dto/UserDto.java
+++ src/main/java/com/example/sendowl/domain/user/dto/UserDto.java
@@ -4,6 +4,7 @@ import com.example.sendowl.domain.user.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -128,4 +129,13 @@ public class UserDto {
         private String email;
         private String token;
     }
+    @Getter
+    @Builder
+    public static class Oauth2Req {
+        @NotBlank
+        private String transactionId;
+        @NotBlank
+        private String token;
+    }
+
 }
