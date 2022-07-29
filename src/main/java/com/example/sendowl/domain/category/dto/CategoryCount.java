c721d4cc88d2ad959a685fa941847811b700eb1b
Dev-Guccin <ghks2047@naver.com>
2022-07-29 14:24:17 +0900

    feat: CategoryCount Dto for jpql

--- src/main/java/com/example/sendowl/domain/category/dto/CategoryCount.java
+++ src/main/java/com/example/sendowl/domain/category/dto/CategoryCount.java
@@ -1,2 +1,25 @@
-package com.example.sendowl.domain.category.dto;public class CategoryCount {
+package com.example.sendowl.domain.category.dto;
+
+import lombok.Getter;
+
+@Getter
+public class CategoryCount {
+    private Long categoryId;
+    private String name;
+    private Long count;
+
+    public CategoryCount(Long categoryId, String name, Long count) {
+        this.categoryId = categoryId;
+        this.name = name;
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return "CategoryCount{" +
+                "categoryId=" + categoryId +
+                ", name='" + name + '\'' +
+                ", count=" + count +
+                '}';
+    }
 }
