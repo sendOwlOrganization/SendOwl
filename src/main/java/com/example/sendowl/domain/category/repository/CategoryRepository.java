16b81d8a168583baff2bceba56e8ae6432c74447
Dev-Guccin <ghks2047@naver.com>
2022-07-29 14:35:51 +0900

    del: remark

--- src/main/java/com/example/sendowl/domain/category/repository/CategoryRepository.java
+++ src/main/java/com/example/sendowl/domain/category/repository/CategoryRepository.java
@@ -11,14 +11,6 @@ import java.util.Optional;
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Optional<Category> findByName(String name);
 
-//    @Query(value =
-//            "select * " +
-//                    "from category as C " +
-//                    "left join " +
-//                    "(select board.category_id, count(*) as count from board group by board.category_id) as B " +
-//                    "on C.category_id = B.category_id " +
-//                    "order by count desc;"
-//    )
     @Query(value = "select new com.example.sendowl.domain.category.dto.CategoryCount(C.id, C.name, count(B))" +
             "from Category C left outer join Board B on C = B.category group by C order by count(B) desc "
     )
