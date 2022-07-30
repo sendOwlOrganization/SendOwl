d2d45f32b14bfbf89d119d81214ed45ab4b2ebbd
Dev-Guccin <ghks2047@naver.com>
2022-07-30 17:49:11 +0900

    feat: UserRepositoryTest test findUserMbtiFromCategory

--- src/test/java/com/example/sendowl/repository/UserRepositoryTest.java
+++ src/test/java/com/example/sendowl/repository/UserRepositoryTest.java
@@ -1,2 +1,39 @@
-package com.example.sendowl.repository;public class UserRepositoryTest {
+package com.example.sendowl.repository;
+
+import com.example.sendowl.domain.category.entity.Category;
+import com.example.sendowl.domain.category.repository.CategoryRepository;
+import com.example.sendowl.domain.user.dto.UserMbti;
+import com.example.sendowl.domain.user.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class UserRepositoryTest {
+
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Test
+    public void 카테고리를기반으로기여한유저의mbti를반환(){
+
+        Optional<Category> category = categoryRepository.findById(2L);
+        System.out.println(category.get().getName());
+
+        List<UserMbti> categoryMbti = userRepository.findUserMbtiFromCategory(category.get());
+        for (UserMbti mbti: categoryMbti
+             ) {
+            System.out.println(mbti.getMbti());
+            System.out.println(mbti.getCount());
+        }
+    }
 }
