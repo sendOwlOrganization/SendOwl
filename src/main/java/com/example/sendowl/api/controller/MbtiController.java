c6a460fe8a109df1db5dc976e80d3868ccd35675
Dev-Guccin <ghks2047@naver.com>
2022-07-30 18:52:12 +0900

    feat: /category/{categoryId} api

--- src/main/java/com/example/sendowl/api/controller/MbtiController.java
+++ src/main/java/com/example/sendowl/api/controller/MbtiController.java
@@ -19,8 +19,14 @@ public class MbtiController {
 
     @Operation(summary = "유저들의 mbti")
     @PostMapping("/users")
-    public BaseResponseDto<List<UserMbti>> getUserByToken() {
+    public BaseResponseDto<List<UserMbti>> getUserMbti() {
         return new BaseResponseDto<>(userService.getUserMbti());
+    }
+
+    @Operation(summary = "카테고리의 유저 mbti", description = "카테고리id를 통해 해당 카테고리에 게시글을 쓴 유저들의 mbti순위(유저수)를 반환한다.")
+    @PostMapping("/category/{categoryId}")
+    public BaseResponseDto<List<UserMbti>> getUserMbtiFromCategory(@PathVariable Long categoryId) {
+        return new BaseResponseDto<>(userService.getUserMbtiFromCategoryId(categoryId));
     }
 
 }
