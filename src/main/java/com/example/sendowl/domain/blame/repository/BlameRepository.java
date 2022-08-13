2e77a4ce540b0b2d5678ab4b0a4d1e58fc3873b0
Dev-Guccin <ghks2047@naver.com>
2022-08-13 19:54:26 +0900

    feat: BlameRepository

--- src/main/java/com/example/sendowl/domain/blame/repository/BlameRepository.java
+++ src/main/java/com/example/sendowl/domain/blame/repository/BlameRepository.java
@@ -1,33 +1,9 @@
-package com.example.sendowl.domain.board.repository;
+package com.example.sendowl.domain.blame.repository;
 
-
-import com.example.sendowl.domain.board.entity.Board;
-
-import com.example.sendowl.domain.category.entity.Category;
-import com.example.sendowl.domain.user.dto.UserMbti;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import com.example.sendowl.domain.blame.Blame;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
-public interface BoardRepository extends JpaRepository<Board, Long>, JpaSpecificationExecutor {
-
-    List<Board> findByIsDeleted(boolean isDeleted);
-
-    @Query(value = "SELECT b FROM Board b join fetch b.user",
-            countQuery = "SELECT COUNT(b) FROM Board b")
-    Page<Board> findAllFetchJoin(Pageable pageable);
-
-    @Query(value = "SELECT b FROM Board b join fetch b.user where b.category.id = :categoryId",
-            countQuery = "SELECT COUNT(b) FROM Board b where b.category.id = :categoryId")
-    Page<Board> findByCategoryIdFetchJoin(Long categoryId, Pageable pageable);
-
-    Page<Board> findByTitleContaining(Pageable pageable, String text);
-    Page<Board> findByContentContaining(Pageable pageable, String text);
-
+public interface BlameRepository extends JpaRepository<Blame, Long>, JpaSpecificationExecutor {
 }
 
