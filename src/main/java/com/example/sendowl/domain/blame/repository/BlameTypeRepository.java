cd9873fef5cb6c56c16ad1098453ef2971c4bf4f
Dev-Guccin <ghks2047@naver.com>
2022-08-13 22:18:20 +0900

    feat: BlameTypeRepositpry

--- src/main/java/com/example/sendowl/domain/blame/repository/BlameTypeRepository.java
+++ src/main/java/com/example/sendowl/domain/blame/repository/BlameTypeRepository.java
@@ -4,6 +4,10 @@ import com.example.sendowl.domain.blame.BlameType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
+
 public interface BlameTypeRepository extends JpaRepository<BlameType, Long>, JpaSpecificationExecutor{
     Boolean existsAllByName(String name);
+
+    Optional<BlameType> findById(Long id);
 }
