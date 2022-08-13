407f82cdc54fa4bf410830367436ce0b074cfa49
Dev-Guccin <ghks2047@naver.com>
2022-08-13 19:52:54 +0900

    feat: BlameService

--- src/main/java/com/example/sendowl/api/service/BlameService.java
+++ src/main/java/com/example/sendowl/api/service/BlameService.java
@@ -1,2 +1,20 @@
-package com.example.sendowl.api.service;public class BlameService {
+package com.example.sendowl.api.service;
+
+import com.example.sendowl.domain.blame.Blame;
+import com.example.sendowl.domain.blame.dto.BlameDto;
+import com.example.sendowl.domain.blame.repository.BlameRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class BlameService {
+    private final BlameRepository blameRepository;
+    public void insertBlame(BlameDto.BlameReq rq){
+
+        Blame blame = rq.toEntity();
+        System.out.println("blame:" + blame.toString());
+        blameRepository.save(blame);
+    }
 }
