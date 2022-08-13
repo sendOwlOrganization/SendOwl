d956ddd0cc9eb89f8b328c386fb9c98b282003e7
Dev-Guccin <ghks2047@naver.com>
2022-08-13 19:53:48 +0900

    feat: Blame Entity

--- src/main/java/com/example/sendowl/domain/blame/Blame.java
+++ src/main/java/com/example/sendowl/domain/blame/Blame.java
@@ -1,28 +1,36 @@
 package com.example.sendowl.domain.blame;
 
 import com.example.sendowl.common.entity.BaseEntity;
-import com.example.sendowl.domain.user.entity.User;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
+@NoArgsConstructor
 public class Blame extends BaseEntity {
     @Id
-    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User userId;
-
-
+    private Long userId;
+    private Long blameType;
     private String blameDetails;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User targetUserId;
-
+    private Long targetUserId;
     private Long contentsType;
     private Long contentsId;
     private String contentsDetails;
+
+    @Builder
+    public Blame(Long id, Long userId, Long blameType, String blameDetails, Long targetUserId, Long contentsType, Long contentsId, String contentsDetails) {
+        this.id = id;
+        this.userId = userId;
+        this.blameType = blameType;
+        this.blameDetails = blameDetails;
+        this.targetUserId = targetUserId;
+        this.contentsType = contentsType;
+        this.contentsId = contentsId;
+        this.contentsDetails = contentsDetails;
+    }
 
 }
