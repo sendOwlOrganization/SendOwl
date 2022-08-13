147659911627d4a6aac07b715f8cae616b90a6a0
Dev-Guccin <ghks2047@naver.com>
2022-08-13 22:03:59 +0900

    feat: BlameDto for insert req

--- src/main/java/com/example/sendowl/domain/blame/dto/BlameDto.java
+++ src/main/java/com/example/sendowl/domain/blame/dto/BlameDto.java
@@ -1,8 +1,10 @@
 package com.example.sendowl.domain.blame.dto;
 
 import com.example.sendowl.domain.blame.Blame;
+import com.example.sendowl.domain.blame.BlameType;
 import com.example.sendowl.domain.board.entity.Board;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -26,6 +28,26 @@ public class BlameDto {
                     .contentsType(contentType)
                     .contentsId(contentId)
                     .contentsDetails(contentDetails).build();
+        }
+    }
+
+    @Getter
+    public static class BlameTypeReq {
+        private String name;
+
+        public BlameType toEntity(){
+            return BlameType.builder().name(name).build();
+        }
+
+    }
+    @Getter
+    public static class BlameTypeRes {
+        private Long id;
+        private String name;
+
+        public BlameTypeRes(BlameType entity) {
+            this.id = entity.getId();
+            this.name = entity.getName();
         }
     }
 }
