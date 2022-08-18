1ab9faad07144155827a0ba619cc421fe332237f
Dev-Guccin <ghks2047@naver.com>
2022-08-18 21:41:07 +0900

    feat: test

--- src/test/java/com/example/sendowl/user/UserServiceTest.java
+++ src/test/java/com/example/sendowl/user/UserServiceTest.java
@@ -1,2 +1,19 @@
-package com.example.sendowl.user;public class UserServiceTest {
+package com.example.sendowl.user;
+
+import com.example.sendowl.api.service.UserService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserServiceTest {
+    @Autowired
+    UserService userService;
+
+    @Test
+    void checkUserNickName(){
+        boolean res = userService.duplicationCheckNickName("t1");
+        Assertions.assertThat(res).isEqualTo(true);
+    }
 }
