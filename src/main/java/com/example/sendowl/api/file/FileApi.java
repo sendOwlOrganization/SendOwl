d3ae84448dd815823c71aea99ebece0e428f3f9d
pis03133 <pis03133@naver.com>
2022-08-20 20:33:31 +0900

    fix: separate file api

--- src/main/java/com/example/sendowl/api/file/FileApi.java
+++ src/main/java/com/example/sendowl/api/file/FileApi.java
@@ -1,12 +1,11 @@
 package com.example.sendowl.api.file;
 
-import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class FileApi {
 
-    public String makeFileFolder(){
+    public String makeFileFolder() throws  Exception {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         Date date = new Date();
         String str = sdf.format(date);
