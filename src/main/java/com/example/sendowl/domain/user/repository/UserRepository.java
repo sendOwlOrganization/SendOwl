bbfe67cb27658b419639d80fb85655f44122a0ce
Dev-Guccin <ghks2047@naver.com>
2022-08-25 19:37:53 +0900

    feat: existsUserByNickName

--- src/main/java/com/example/sendowl/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/sendowl/domain/user/repository/UserRepository.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsUserByEmail(String email);
+    boolean existsUserByNickName(String nickName);
     boolean existsUserByEmailAndTransactionId(String email, String trancationId);
 
     boolean existsUserByNickName(String nickName);
