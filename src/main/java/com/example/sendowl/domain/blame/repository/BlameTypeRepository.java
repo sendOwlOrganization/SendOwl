00b6d8e7369de8ba5fc8924effd8b83a56d001da
Dev-Guccin <ghks2047@naver.com>
2022-08-28 17:52:55 +0900

    feat: findAllByIsDeletedFalse

--- src/main/java/com/example/sendowl/domain/blame/repository/BlameTypeRepository.java
+++ src/main/java/com/example/sendowl/domain/blame/repository/BlameTypeRepository.java
@@ -4,10 +4,12 @@ import com.example.sendowl.domain.blame.BlameType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BlameTypeRepository extends JpaRepository<BlameType, Long>, JpaSpecificationExecutor{
     Boolean existsAllByName(String name);
 
+    List<BlameType> findAllByIsDeletedFalse();
     Optional<BlameType> findById(Long id);
 }
