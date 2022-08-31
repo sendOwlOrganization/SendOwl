0fdf71c101207d1d78c0b193005eb4cc75e6bb1c
Dev-Guccin <ghks2047@naver.com>
2022-08-31 21:29:02 +0900

    feat: BalanceErrorCode

--- src/main/java/com/example/sendowl/domain/balance/exception/enums/BalanceErrorCode.java
+++ src/main/java/com/example/sendowl/domain/balance/exception/enums/BalanceErrorCode.java
@@ -7,8 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum BalanceErrorCode implements BaseErrorCodeIF {
 
-    ALREADYEXIST(HttpStatus.BAD_REQUEST, "이미 등록된 신고 타입입니다."),
-    NOTFOUND(HttpStatus.NOT_FOUND, "등록되지 않은 신고 타입입니다."),
+    NOTFOUND(HttpStatus.NOT_FOUND, "등록되지 않은 밸런스입니다."),
     ;
     private final HttpStatus errorStatus;
     private final String errorMessage;
