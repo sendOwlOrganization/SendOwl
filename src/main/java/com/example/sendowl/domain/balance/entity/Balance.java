d05bee62e33cc8f06640ff1268a36c1c60da02ae
Dev-Guccin <ghks2047@naver.com>
2022-08-31 21:27:38 +0900

    feat: Balance BalanceCheck entity

--- src/main/java/com/example/sendowl/domain/balance/entity/Balance.java
+++ src/main/java/com/example/sendowl/domain/balance/entity/Balance.java
@@ -1,2 +1,60 @@
-package com.example.sendowl.domain.balance.entity;public class Balance {
+package com.example.sendowl.domain.balance.entity;
+
+import com.example.sendowl.common.entity.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Balance extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String title;
+    private String aDetail;
+    private String bDetail;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setaDetail(String aDetail) {
+        this.aDetail = aDetail;
+    }
+
+    public void setbDetail(String bDetail) {
+        this.bDetail = bDetail;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getaDetail() {
+        return aDetail;
+    }
+
+    public String getbDetail() {
+        return bDetail;
+    }
+
+    @Override
+    public String toString() {
+        return "Balance{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", aDetail='" + aDetail + '\'' +
+                ", bDetail='" + bDetail + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/example/sendowl/domain/balance/entity/BalanceCheck.java
+++ src/main/java/com/example/sendowl/domain/balance/entity/BalanceCheck.java
@@ -1,2 +1,33 @@
-package com.example.sendowl.domain.balance.entity;public class BalanceCheck {
+package com.example.sendowl.domain.balance.entity;
+
+import com.example.sendowl.common.entity.BaseEntity;
+import com.example.sendowl.domain.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BalanceCheck extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "balance_id")
+    private Balance balance;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+    private String pick;
+
+    public void setPick(String pick) {
+        this.pick = pick;
+    }
 }
