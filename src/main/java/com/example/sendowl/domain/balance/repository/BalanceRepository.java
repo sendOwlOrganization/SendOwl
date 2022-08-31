747012fe23bc73dd17e385a6f4a646f92f00ac53
Dev-Guccin <ghks2047@naver.com>
2022-08-31 21:30:36 +0900

    feat: Balance Repository

--- src/main/java/com/example/sendowl/domain/balance/repository/BalanceRepository.java
+++ src/main/java/com/example/sendowl/domain/balance/repository/BalanceRepository.java
@@ -1,2 +1,13 @@
-package com.example.sendowl.domain.balance.repository;public class BalanceRepository {
+package com.example.sendowl.domain.balance.repository;
+
+import com.example.sendowl.domain.balance.entity.Balance;
+import com.example.sendowl.domain.balance.entity.BalanceCheck;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.List;
+
+public interface BalanceRepository  extends JpaRepository<Balance, Long>, JpaSpecificationExecutor {
+
+    List<Balance> findTop10ByIsDeletedFalseOrderByRegDateDesc();
 }
