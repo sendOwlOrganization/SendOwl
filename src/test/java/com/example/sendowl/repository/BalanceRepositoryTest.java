7b56577ab990b2185dac440af81bd83b151f603a
Dev-Guccin <ghks2047@naver.com>
2022-09-03 15:54:10 +0900

    feat: BalanceCountTest

--- src/test/java/com/example/sendowl/repository/BalanceRepositoryTest.java
+++ src/test/java/com/example/sendowl/repository/BalanceRepositoryTest.java
@@ -1,2 +1,28 @@
-package com.example.sendowl.repository;public class BalanceRepositoryTest {
+package com.example.sendowl.repository;
+
+import com.example.sendowl.domain.balance.dto.BalanceCount;
+import com.example.sendowl.domain.balance.repository.BalanceRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.List;
+
+@DataJpaTest(
+        properties = {"spring.jpa.properties.hibernate.default_batch_fetch_size=1000"}
+)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class BalanceRepositoryTest {
+    @Autowired
+    private BalanceRepository balanceRepository;
+
+    @Test
+    public void BalanceCountTest(){
+        List<BalanceCount> balanceCounts = balanceRepository.getBalanceCount(PageRequest.of(0,10));
+
+        balanceCounts.forEach(balanceCount ->
+                System.out.println(balanceCount.getId() + " / " +balanceCount.getTitle() + "/" + balanceCount.getACount() +" / " + balanceCount.getBCount()) );
+    }
 }
