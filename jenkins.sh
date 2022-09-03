09c1cb76ff0f85f144bcc8d502df152803543c42
Dev-Guccin <52245525+Dev-Guccin@users.noreply.github.com>
2022-09-03 15:04:52 +0900

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,3 +1,3 @@
-sudo kill -9 `ps -ef| grep build/libs/SendOwl-0.0.1-SNAPSHOT.jar | awk '{print $2}'` &
-sudo java -jar build/libs/SendOwl-0.0.1-SNAPSHOT.jar &
+sudo kill -9 `ps -ef| grep build/libs/SendOwl-0.0.1-SNAPSHOT.jar | awk '{print $2}'`
+sudo nohup java -jar build/libs/SendOwl-0.0.1-SNAPSHOT.jar 1>sendowllog.out 2>sendowllog.err &
 exit 0
