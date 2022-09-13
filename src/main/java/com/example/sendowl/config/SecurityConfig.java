3acf65401b97cd73b79d4caa7aa919cbd4eb82a5
Dev-Guccin <ghks2047@naver.com>
2022-09-13 21:47:50 +0900

    del: unneccessary lib

--- src/main/java/com/example/sendowl/config/SecurityConfig.java
+++ src/main/java/com/example/sendowl/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import com.example.sendowl.domain.user.entity.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
