db90e139e246669daf752a502f3d4b24798c0c26
Dev-Guccin <ghks2047@naver.com>
2022-10-06 20:22:37 +0900

    feat: NoPermission handler

--- src/main/java/com/example/sendowl/domain/comment/exception/CommentNoPermission.java
+++ src/main/java/com/example/sendowl/domain/comment/exception/CommentNoPermission.java
@@ -3,13 +3,13 @@ package com.example.sendowl.domain.comment.exception;
 import com.example.sendowl.common.exception.BaseException;
 import com.example.sendowl.common.exception.enums.BaseErrorCodeIF;
 
-public class CommentNotFoundException extends BaseException {
+public class CommentNoPermission extends BaseException {
 
-    public CommentNotFoundException(BaseErrorCodeIF errorCode) {
+    public CommentNoPermission(BaseErrorCodeIF errorCode) {
         super(errorCode);
     }
 
-    public CommentNotFoundException(BaseErrorCodeIF errorCode, Throwable throwable) {
+    public CommentNoPermission(BaseErrorCodeIF errorCode, Throwable throwable) {
         super(errorCode, throwable);
     }
 
