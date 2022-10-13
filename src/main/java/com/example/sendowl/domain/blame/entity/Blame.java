597479259f909b2b5d847a458c3210251b8ad101
Dev-Guccin <ghks2047@naver.com>
2022-10-13 20:14:15 +0900

    fix: userId to User user and targetUserId to User targetUser

--- src/main/java/com/example/sendowl/domain/blame/entity/Blame.java
+++ src/main/java/com/example/sendowl/domain/blame/entity/Blame.java
@@ -1,6 +1,7 @@
 package com.example.sendowl.domain.blame.entity;
 
 import com.example.sendowl.common.entity.BaseEntity;
+import com.example.sendowl.domain.user.entity.User;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
@@ -13,21 +14,27 @@ public class Blame extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
-    private Long userId;
-    private Long blameType;
-    private String blameDetails;
-    private Long targetUserId;
-    private Long contentsType;
-    private Long contentsId;
-    private String contentsDetails;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+    private Long blameType; // 신고 종류
+    private String blameDetails; // 신고 내용
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "target_user_id")
+    private User targetUser; // 신고당하는 사람 id
+    private Long contentsType; // 게시글:1, 댓글:2 ...
+    private Long contentsId; // 해당 콘텐츠의 id
+    private String contentsDetails; // 신고하는 내용
 
     @Builder
-    public Blame(Long id, Long userId, Long blameType, String blameDetails, Long targetUserId, Long contentsType, Long contentsId, String contentsDetails) {
+    public Blame(Long id, User user, Long blameType, String blameDetails, User targetUser, Long contentsType, Long contentsId, String contentsDetails) {
         this.id = id;
-        this.userId = userId;
+        this.user = user;
         this.blameType = blameType;
         this.blameDetails = blameDetails;
-        this.targetUserId = targetUserId;
+        this.targetUser = targetUser;
         this.contentsType = contentsType;
         this.contentsId = contentsId;
         this.contentsDetails = contentsDetails;
