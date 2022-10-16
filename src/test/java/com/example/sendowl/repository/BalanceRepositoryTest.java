8eb8bece33d6859299681b1597a67faac1edf7d9
Dev-Guccin <ghks2047@naver.com>
2022-10-16 14:46:20 +0900

    test: GetDetailBalanceCountTest for BalanceRepositoryTest

--- src/test/java/com/example/sendowl/repository/BalanceRepositoryTest.java
+++ src/test/java/com/example/sendowl/repository/BalanceRepositoryTest.java
@@ -3,12 +3,15 @@ package com.example.sendowl.repository;
 import com.example.sendowl.domain.balance.dto.BalanceCount;
 import com.example.sendowl.domain.balance.repository.BalanceRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
+import java.util.Optional;
 
 @DataJpaTest(
         properties = {"spring.jpa.properties.hibernate.default_batch_fetch_size=1000"}
@@ -19,10 +22,15 @@ public class BalanceRepositoryTest {
     private BalanceRepository balanceRepository;
 
     @Test
-    public void BalanceCountTest(){
-        List<BalanceCount> balanceCounts = balanceRepository.getBalanceCount(PageRequest.of(0,10));
+    public void GetAllBalanceCountTest(){
+        List<BalanceCount> balanceCounts = balanceRepository.getAllBalanceCount(PageRequest.of(0,10));
 
         balanceCounts.forEach(balanceCount ->
                 System.out.println(balanceCount.getId() + " / " +balanceCount.getTitle() + "/" + balanceCount.getACount() +" / " + balanceCount.getBCount()) );
+    }
+    @Test
+    public void GetDetailBalanceCountTest(){
+        Optional<BalanceCount> balanceCount = balanceRepository.getBalanceCount(1L);
+        balanceCount.get().toString();
     }
 }
