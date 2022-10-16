9ae78a7e797351fb2445f39f8cd1fe69db8cef03
Dev-Guccin <ghks2047@naver.com>
2022-10-16 14:45:52 +0900

    feat: getBalanceCount in BalanceRepository

--- src/main/java/com/example/sendowl/domain/balance/repository/BalanceRepository.java
+++ src/main/java/com/example/sendowl/domain/balance/repository/BalanceRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BalanceRepository  extends JpaRepository<Balance, Long>, JpaSpecificationExecutor {
     @Query(value = "select new com.example.sendowl.domain.balance.dto.BalanceCount(" +
@@ -25,6 +26,13 @@ public interface BalanceRepository  extends JpaRepository<Balance, Long>, JpaSpe
             "group by B " +
             "order by B.regDate desc"
     )
-    List<BalanceCount> getBalanceCount(Pageable pageable);
+    List<BalanceCount> getAllBalanceCount(Pageable pageable);
 
+    @Query(value = "select new com.example.sendowl.domain.balance.dto.BalanceCount(" +
+            "B.id, B.title, B.aDetail, B.bDetail, " +
+            "sum(case when BC.pick='A' then 1 else 0 end), " +
+            "sum(case when BC.pick='B' then 1 else 0 end)) " +
+            "from Balance B left join BalanceCheck BC on B = BC.balance " +
+            "where B.isDeleted=false and B.id=:balanceId")
+    Optional<BalanceCount> getBalanceCount(Long balanceId);
 }
