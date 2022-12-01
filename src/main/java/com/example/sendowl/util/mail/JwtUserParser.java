4856e7f5020054412bb35911d7a7ee9cb5a7e500
Dev-Guccin <ghks2047@naver.com>
2022-12-01 19:51:09 +0900

    feat: JwtUserParser for getting user from jwt more easily

--- src/main/java/com/example/sendowl/util/mail/JwtUserParser.java
+++ src/main/java/com/example/sendowl/util/mail/JwtUserParser.java
@@ -1,2 +1,16 @@
-package com.example.sendowl.util.mail;public class JwtUserParser {
+package com.example.sendowl.util.mail;
+
+import com.example.sendowl.auth.PrincipalDetails;
+import com.example.sendowl.domain.user.entity.User;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUserParser {
+
+    public User getUser() {
+        PrincipalDetails principal = (PrincipalDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return principal.getUser();
+    }
+
 }
