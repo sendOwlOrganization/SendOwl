a331437e81a6f831b012b95372f0072cdd50c811
Dev-Guccin <52245525+Dev-Guccin@users.noreply.github.com>
2022-12-04 22:51:45 +0900

    Update SendOwlApplication.java

--- src/main/java/com/example/sendowl/SendOwlApplication.java
+++ src/main/java/com/example/sendowl/SendOwlApplication.java
@@ -3,7 +3,6 @@ package com.example.sendowl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @RequiredArgsConstructor
