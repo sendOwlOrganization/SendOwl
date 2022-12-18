fa18c0f3267a7ebf19ef2f5d9be96c316932f616
LeeJaeYeon <dlwodusqmdl@naver.com>
2022-12-18 21:14:22 +0900

    로그인 여부에 따라 DBLogging

--- src/main/java/com/example/sendowl/common/aop/DbLoggingAop.java
+++ src/main/java/com/example/sendowl/common/aop/DbLoggingAop.java
@@ -25,21 +25,29 @@ public class DbLoggingAop {
     // 게시물 상세조회시
     @Around("execution(* com.example.sendowl.api.service.BoardService.getBoardList(..))")
     public Object boardLog (ProceedingJoinPoint joinPoint) throws Throwable {
+
         try{
             // get Parameters from HttpRequest
             HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
             //Map<String,String[]> params = request.getParameterMap();
 
-            // get UserInfo
-            PrincipalDetails principal = (PrincipalDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-            User user = principal.getUser();
 
-            // call service
-            boardLogsService.insertBoardKeywordLog(Long.parseLong(request.getParameter("categoryId")), user.getMbti());
-//            System.out.println("This is Aop");
+            if(!(SecurityContextHolder.getContext().getAuthentication().getPrincipal().equals("anonymousUser"))){
+
+                // 유저 정보가 있을때만 insert
+                // get UserInfo
+                PrincipalDetails principal = (PrincipalDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+                User user = principal.getUser();
+
+
+
+                // call service
+                boardLogsService.insertBoardKeywordLog(Long.parseLong(request.getParameter("categoryId")), user.getMbti());
+            }
+            // 없으면 진행
             return joinPoint.proceed();
-
-        } finally {
+        }
+        finally {
 
         }
     }
