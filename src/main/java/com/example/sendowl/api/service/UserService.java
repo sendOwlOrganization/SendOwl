8749249159e9d0d0a3504854a18920259f9f0e12
Dev-Guccin <ghks2047@naver.com>
2023-02-06 22:32:01 +0900

    feat: cors add Header Access-Control-Expose-Headers

--- src/main/java/com/example/sendowl/api/service/UserService.java
+++ src/main/java/com/example/sendowl/api/service/UserService.java
@@ -154,11 +154,10 @@ public class UserService {
             alreadySetted = false;
         }
         // 로그인 (토큰 반환)
-//        makeToken(
-//                userRepository.findByEmailAndTransactionId(user.getEmail(), user.getTransactionId()).get()
-//        ).forEach(servletResponse::addHeader);
-        HashMap<String, String> hashMap = makeToken(userRepository.findUserByEmailAndTransactionId(user.getEmail(), user.getTransactionId()).get());
-        servletResponse.addHeader("Access-Control-Expose-Headers", hashMap.get(ACCESS_TOKEN) + "," + hashMap.get(REFRESH_TOKEN));
+        makeToken(
+                userRepository.findByEmailAndTransactionId(user.getEmail(), user.getTransactionId()).get()
+        ).forEach(servletResponse::addHeader);
+        servletResponse.addHeader("Access-Control-Expose-Headers", "accessToken");
 
         return new Oauth2Res(alreadyJoined, alreadySetted, retUser);
     }
