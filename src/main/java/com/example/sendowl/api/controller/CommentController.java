a3dfa96278d6d34faed38e2e1a7757fc01affbb8
LeeJaeYeon <dlwodusqmdl@naver.com>
2023-02-06 18:29:50 +0900

    feature: swagger UI

--- src/main/java/com/example/sendowl/api/controller/CommentController.java
+++ src/main/java/com/example/sendowl/api/controller/CommentController.java
@@ -4,10 +4,13 @@ import com.example.sendowl.api.service.CommentService;
 import com.example.sendowl.auth.PrincipalDetails;
 import com.example.sendowl.domain.user.entity.User;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -38,8 +41,10 @@ public class CommentController {
 
     @Operation(summary = "댓글 목록 조회", description = "게시글의 id를 통해 댓글 목록을 가져온다.")
     @GetMapping(path = "/{boardId}") // 댓글 목록
-
-    public ResponseEntity<?> getCommentList(@RequestParam("board-id") Long boardId, Pageable pageable){
+    public ResponseEntity<?> getCommentList(@RequestParam(name="board-id") Long boardId,
+                                            @PageableDefault(sort="regDate", direction = Sort.Direction.DESC, size = 10, page = 0)
+                                            @Parameter(required = false, description = "example: {\"page\": 0,\"size\": 1,\n" +
+                                                    "  \"sort\": [\"regDate,desc\"]\n" + "}") Pageable pageable){
         Page<CommentRes> commentResList = commentService.selectCommentList(boardId, pageable);
 
         return new ResponseEntity(commentResList, HttpStatus.OK);
