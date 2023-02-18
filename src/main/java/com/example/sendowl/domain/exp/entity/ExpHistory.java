b543a3f536b6744e452ff44adcbc6881e9607c83
Dev-Guccin <ghks2047@naver.com>
2023-02-18 15:18:51 +0900

    feat: add expHistory entity && expType

---
+++ src/main/java/com/example/sendowl/domain/exp/entity/ExpHistory.java
@@ -0,0 +1,31 @@
+package com.example.sendowl.domain.exp.entity;
+
+import com.example.sendowl.common.entity.BaseEntity;
+import com.example.sendowl.domain.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ExpHistory extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // null 넣으면 DB가 알아서 autoincrement해준다.
+    @Column(name = "user_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private ExpType type;
+
+    private Long addExp;
+}
