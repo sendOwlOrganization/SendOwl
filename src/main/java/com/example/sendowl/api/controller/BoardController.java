c71e860dbff32bd81a028647b99cc60d714a40fa
Dev-Guccin <ghks2047@naver.com>
2023-03-12 18:31:30 +0900

    feat: BoardController Swagger update #136

--- src/main/java/com/example/sendowl/api/controller/BoardController.java
+++ src/main/java/com/example/sendowl/api/controller/BoardController.java
@@ -52,8 +52,21 @@ public class BoardController {
 
     @Operation(summary = "게시글 목록 조회", description = "조건에 따라 게시글을 조회한다." +
             "http://localhost:8080/api/boards?categoryId=0&page=0&size=1&sort=id,DESC")
+    @Parameters({
+            @Parameter(name = "categoryId", example = "1", description = "게시글의 카테고리 id"),
+            @Parameter(name = "textLength", example = "10", description = "게시글 제목의 글자수 제한"),
+            @Parameter(name = "pageable", example = "{\"page\": 0, \"size\": 10, \"sort\": [\"reg_date,DESC\"]}"
+                    , description = "페이지네이션을 위한 옵션")
+    })
     @GetMapping(path = "") // 게시글 목록
-    public ResponseEntity<?> boards(final @RequestParam Long categoryId, final @RequestParam Integer textLength, Pageable pageable) {
+    public ResponseEntity<?> boards(
+            final @RequestParam Long categoryId,
+            final @RequestParam Integer textLength,
+            @PageableDefault(
+                    size = 20,
+                    sort = "board_id",
+                    direction = Sort.Direction.DESC) Pageable pageable
+    ) {
         BoardsRes boardsRes = boardService.getBoardList(categoryId, textLength, pageable);
 
         return new ResponseEntity(boardsRes, HttpStatus.OK);
@@ -62,16 +75,19 @@ public class BoardController {
     @PreAuthorize("hasAuthority('USER')")
     @Operation(summary = "게시글 삽입", description = "새로운 게시글을 삽입한다.", security = {@SecurityRequirement(name = "bearerAuth")})
     @PostMapping(path = "", produces = "application/json; charset=utf8") // 게시글 등록
-    public @ResponseBody ResponseEntity<?> board(final @Valid @RequestBody BoardReq rq) {
+    public ResponseEntity<?> board(final @Valid @RequestBody BoardReq boardReq) {
 
         PrincipalDetails principal = (PrincipalDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         User user = principal.getUser();
-        DetailRes detailRes = boardService.insertBoard(rq, user);
+        DetailRes detailRes = boardService.insertBoard(boardReq, user);
 
         return new ResponseEntity(detailRes, HttpStatus.OK);
     }
 
     @Operation(summary = "게시글 상세 조회", description = "게시글을 상세 조회한다.")
+    @Parameters({
+            @Parameter(name = "id", example = "1", description = "게시글의 id"),
+    })
     @GetMapping(path = "/{id}") // 게시글 상세
     public ResponseEntity<?> boardDetail(@PathVariable Long id) {
         DetailRes detailRes = boardService.boardDetail(id);
@@ -93,6 +109,9 @@ public class BoardController {
 
     @PreAuthorize("hasAuthority('USER')")
     @Operation(summary = "게시글 소프트 삭제", description = "게시글을 소프트 삭제한다.", security = {@SecurityRequirement(name = "bearerAuth")})
+    @Parameters({
+            @Parameter(name = "id", example = "1", description = "게시글의 id"),
+    })
     @DeleteMapping(path = "/{id}") // 게시글 삭제
     public ResponseEntity<?> boardDelete(@PathVariable Long id) {
         PrincipalDetails principal = (PrincipalDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
