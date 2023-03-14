96de9f68a166d20e380f1bf166698b8366f0641e
Dev-Guccin <ghks2047@naver.com>
2023-03-14 21:58:06 +0900

    feat: CommentController Swagger update #136

--- src/main/java/com/example/sendowl/api/controller/CommentController.java
+++ src/main/java/com/example/sendowl/api/controller/CommentController.java
@@ -18,8 +18,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 import static com.example.sendowl.domain.comment.dto.CommentDto.*;
 
 @RestController
@@ -30,9 +28,9 @@ public class CommentController {
     private final CommentService commentService;
 
     @PreAuthorize("hasAuthority('USER')")
-    @Operation(summary = "댓글 삽입", description = "댓글을 삽입한다.", security = { @SecurityRequirement(name = "bearerAuth") })
+    @Operation(summary = "댓글 삽입", description = "댓글을 삽입한다.", security = {@SecurityRequirement(name = "bearerAuth")})
     @PostMapping(path = "") // 댓글 등록
-    public ResponseEntity<?> insertComment(@RequestBody CommentReq vo){
+    public ResponseEntity<?> insertComment(@RequestBody CommentReq vo) {
         PrincipalDetails principal = (PrincipalDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         User user = principal.getUser();
         CommentRes commentRes = this.commentService.insertComment(vo, user);
@@ -41,32 +39,38 @@ public class CommentController {
     }
 
     @Operation(summary = "댓글 목록 조회", description = "게시글의 id를 통해 댓글 목록을 가져온다.")
-    @Parameters(@Parameter(name = "pageable", required = false, description = "example: {\"page\": 0,\"size\": 1,\n" +
-            "  \"sort\": [\"regDate,desc\"]\n" + "}"))
+    @Parameters({
+            @Parameter(name = "boardId", required = true, description = "게시글 id", example = "1"),
+            @Parameter(name = "pageable", required = false, description = "example: {\"page\": 0,\"size\": 1,\n" +
+                    "  \"sort\": [\"regDate,desc\"]\n" + "}")
+    })
     @GetMapping(path = "") // 댓글 목록
-    public ResponseEntity<?> getCommentList(@RequestParam(name="boardId") Long boardId,
-                                            @PageableDefault(sort="regDate", direction = Sort.Direction.DESC, size = 10, page = 0) Pageable pageable){
+    public ResponseEntity<?> getCommentList(@RequestParam(name = "boardId") Long boardId,
+                                            @PageableDefault(sort = "regDate", direction = Sort.Direction.DESC, size = 10, page = 0) Pageable pageable) {
         Page<CommentRes> commentResList = commentService.selectCommentList(boardId, pageable);
 
         return new ResponseEntity(commentResList, HttpStatus.OK);
     }
 
     @PreAuthorize("hasAuthority('USER')")
-    @Operation(summary = "댓글 내용 수정", description = "댓글의 내용을 수정한다.", security = { @SecurityRequirement(name = "bearerAuth") })
+    @Operation(summary = "댓글 내용 수정", description = "댓글의 내용을 수정한다.", security = {@SecurityRequirement(name = "bearerAuth")})
     @PutMapping(path = "") // 댓글 내용 수정
-    public ResponseEntity<?> updateComment(UpdateReq crq){
+    public ResponseEntity<?> updateComment(UpdateReq updateReq) {
         PrincipalDetails principal = (PrincipalDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         User user = principal.getUser();
 
-        CommentRes commentRes = commentService.updateComment(crq,user);
+        CommentRes commentRes = commentService.updateComment(updateReq, user);
 
         return new ResponseEntity(commentRes, HttpStatus.OK);
     }
 
     @PreAuthorize("hasAuthority('USER')")
-    @Operation(summary = "댓글 소프트 삭제", description = "댓글을 소프트 삭제한다.", security = { @SecurityRequirement(name = "bearerAuth") })
-    @DeleteMapping(path = "{commentId}") // 댓글 삭제
-    public ResponseEntity<?> deleteComment(@RequestParam("comment-id") Long commentId){
+    @Operation(summary = "댓글 소프트 삭제", description = "댓글을 소프트 삭제한다.", security = {@SecurityRequirement(name = "bearerAuth")})
+    @Parameters(
+            @Parameter(name = "commentId", required = true, example = "1", description = "삭제할 댓글 id")
+    )
+    @DeleteMapping(path = "/{commentId}") // 댓글 삭제
+    public ResponseEntity<?> deleteComment(@PathVariable("commentId") Long commentId) {
         PrincipalDetails principal = (PrincipalDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         User user = principal.getUser();
 
