c2199df1d52d992dc39fc56622c544559dc87065
Dev-Guccin <ghks2047@naver.com>
2023-03-30 00:42:41 +0900

    feat: refactoring Board domain

--- src/main/java/com/example/sendowl/api/service/BoardService.java
+++ src/main/java/com/example/sendowl/api/service/BoardService.java
@@ -14,6 +14,7 @@ import com.example.sendowl.domain.category.repository.CategoryRepository;
 import com.example.sendowl.domain.user.entity.User;
 import com.example.sendowl.domain.user.exception.UserException.UserUnauthorityException;
 import com.example.sendowl.domain.user.exception.enums.UserErrorCode;
+import com.example.sendowl.util.mail.JwtUserParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -35,6 +36,8 @@ public class BoardService {
     private final BoardRepository boardRepository;
     private final CategoryRepository categoryRepository;
     private final ExpService expService;
+    private final EditorJsHelper editorJsHelper;
+    private final JwtUserParser jwtUserParser;
 
     public List<PreviewBoardRes> getPreviewBoardList(Long categoryId, Integer titleLength, Pageable pageable) {
         Category category = categoryRepository.findById(categoryId).orElseThrow(
@@ -49,8 +52,12 @@ public class BoardService {
     }
 
     public BoardsRes getBoardList(Long categoryId, Integer textLength, Pageable pageable) {
+        Category category = categoryRepository.findById(categoryId).orElseThrow(
+                () -> new CategoryNotFoundException(CategoryErrorCode.NOT_FOUND)
+        );
+
         Page<Board> pages;
-        if (categoryId == 0L) {
+        if (category.getId() == 0L) {
             pages = boardRepository.findBoardFetchJoin(pageable);
         } else {
             pages = boardRepository.findBoardByCategoryIdFetchJoin(categoryId, pageable);
@@ -74,12 +81,13 @@ public class BoardService {
     }
 
     @Transactional
-    public DetailRes insertBoard(BoardReq req, User user) {
+    public DetailRes insertBoard(BoardReq req) {
+        User user = jwtUserParser.getUser();
+
         Category category = categoryRepository.findById(req.getCategoryId())
                 .orElseThrow(() -> new CategoryNotFoundException(CategoryErrorCode.NOT_FOUND));
 
-        //String refinedText = new MarkdownToText(req.getContent()).getRefinedText();
-        String refinedText = new EditorJsHelper().extractText(req.getEditorJsContent());
+        String refinedText = editorJsHelper.extractText(req.getEditorJsContent());
 
         Board savedBoard = boardRepository.save(req.toEntity(user, category, refinedText));
         expService.addExpBoard(user);
