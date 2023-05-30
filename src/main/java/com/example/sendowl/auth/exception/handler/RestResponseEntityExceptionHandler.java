333938544a1b094acaab6d39827bcab9fb8105e2
Dev-Guccin <ghks2047@naver.com>
2023-05-30 22:25:26 +0900

    feat: 검증 로직에 대한 예외처리

--- src/main/java/com/example/sendowl/auth/exception/handler/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/example/sendowl/auth/exception/handler/RestResponseEntityExceptionHandler.java
@@ -3,24 +3,43 @@ package com.example.sendowl.auth.exception.handler;
 import com.example.sendowl.auth.exception.JwtInvalidException;
 import com.example.sendowl.auth.exception.enums.TokenErrorCode;
 import com.example.sendowl.common.dto.BaseErrorResponseDto;
+import com.example.sendowl.common.exception.BaseException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.HashMap;
+import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice(basePackages = "com.example.sendowl.api.controller")
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
-    // @PreAuthorize 의 예외를 핸들링하기 위한 클래스
-    // ResponseEntityExceptionHandler 은 spring mvc에서 발생하는 전반적인 예외들을 handling 한다.
+    // ResponseEntityExceptionHandler는 스프링 프레임워크에서 제공하는 기본적인 예외 처리 클래스
 
+    // @PreAuthorize 의 예외를 핸들링하기 위한 클래스
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<BaseErrorResponseDto> handleAccessDeniedException(Exception ex, WebRequest request) {
         JwtInvalidException boxedException = new JwtInvalidException(
                 TokenErrorCode.FORBIDDEN, ex);
         return new ResponseEntity<>(BaseErrorResponseDto.of(boxedException),
+                boxedException.getErrorStatus());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        Map<String, String> validErrors = new HashMap<>();
+        ex.getBindingResult().getAllErrors()
+                .forEach(c -> validErrors.put(((FieldError) c).getField(), c.getDefaultMessage()));
+
+        BaseException boxedException = new BaseException(status, ex.getMessage(), ex.getCause());
+        return new ResponseEntity<>(
+                BaseErrorResponseDto.ofMap(boxedException, validErrors),
                 boxedException.getErrorStatus());
     }
 }
